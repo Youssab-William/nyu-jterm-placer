0e2f7c1c062dbff21d2b6f4a099879e1a3a2f4d7
alhussni-aa <114022426+alhussni-aa@users.noreply.github.com>
2022-11-12 06:57:34 -0500

    Add files via upload

---
+++ student.cpp
@@ -0,0 +1,19 @@
+#include "../include/student.h"
+
+#include <iostream>
+#include <string>
+
+std::ostream& operator<<(std::ostream& os, const Student &student) {
+  os << "{" << student.name << ", ";
+  os << std::to_string(student.year) << ", ";
+  os << "[";
+  for (int i = 0; i < (int)student.choices.size(); i++) {
+    if (i) os << ", ";
+    os << student.choices[i];
+  }
+  os << "]";
+  os << ", " << student.placement;
+  os << "}";
+
+  return os;
+}
